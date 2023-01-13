b7bd668eff6def00140aeb65c6acb00ae3670403
vladislav shakhov <shahov199@gmail.com>
2023-01-13 17:33:08 +0300

    Zadacha_27

---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int N = ReadInt("Enter N");
+sum (N);
+void sum (int N)
+{
+    int result = 0;
+    while (N > 0)
+    {
+        result = result + N % 10;
+        N /= 10;
+    }
+    Console.WriteLine(result);
+}
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
