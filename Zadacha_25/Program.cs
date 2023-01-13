31bb2072faf5d8ed71fec952f342c3f83850a335
vladislav shakhov <shahov199@gmail.com>
2023-01-13 17:23:22 +0300

    zadacha_25

---
+++ Zadacha_25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и В) и 
+// возводит число А в натуральную степень В.
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+int A = ReadInt("Enter A");
+int B = ReadInt("Enter B");
+ToDegree(A, B);
+void ToDegree (int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    Console.WriteLine(result);
+}
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
